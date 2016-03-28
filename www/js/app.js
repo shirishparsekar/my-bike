aa74948e0bab68cdaa9d8efaa5aa975d88e935ca
shirishparsekar <parsekarshirish171@gmail.com>
2016-03-28 09:55:20 +0530

    final enhancements to v1.0

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'LocalStorageModule']
     $ionicPlatform.ready(function () {
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+            //cordova.plugins.Keyboard.disableScroll(true);
 
         }
         if (window.StatusBar) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,15 +42,24 @@ angular.module('starter.controllers', [])
 })
 
 .controller('RecordsCtrl', function ($scope, localStorageService, $location, $anchorScroll, $ionicPopover) {
-    $scope.petrol = "";
-    $scope.meter = "";
-    $scope.rate = "";
+    $scope.petrol = null;
+    $scope.meter = null;
+    $scope.rate = null;
     $scope.editableField = "";
     $scope.records = localStorageService.get("mywheels_records");
 
-    $scope.petrolEdit = "";
-    $scope.meterEdit = "";
-    $scope.rateEdit = "";
+    $scope.clearFields = function () {
+        $scope.petrol = null;
+        $scope.meter = null;
+        $scope.rate = null;
+    }
+
+    var input_title_add_new_record = "Add new record";
+    var input_title_edit_record = "Edit Record";
+    $scope.inputTitle = input_title_add_new_record;
+    $scope.input_edit_record = false;
+
+
     if ($scope.records === null) {
         $scope.records = [];
     }
@@ -78,22 +87,35 @@ angular.module('starter.controllers', [])
         localStorageService.set("mywheels_records", $scope.records);
     }
 
-    $scope.editField = function ($event, index, petrol, meter, rate) {
-        $scope.petrolEdit = petrol;
-        $scope.meterEdit = meter;
-        $scope.rateEdit = rate;
-        $scope.recordIndex = index;
+    var switchInputToNewRecord = function () {
+        $scope.editableField = null;
+        $scope.input_edit_record = false;
+        $scope.inputTitle = input_title_add_new_record;
+        $scope.clearFields();
+    }
 
-        $ionicPopover.fromTemplateUrl("popover", {
-            scope: $scope
-        }).then(function (popover) {
-            $scope.popover = popover;
-            $scope.popover.show($event);
-        });
+    var switchInputToEditField = function () {
+        //not yet implemented.. for now, code is in else part
+    }
+
+    $scope.editField = function ($event, index, petrol, meter, rate) {
+        if ($scope.editableField === index) {
+            switchInputToNewRecord();
+        }
+        else {
+            switchInputToEditField();
+            $scope.editableField = index;
+            $scope.recordIndex = index;
+            $scope.input_edit_record = true;
+            $scope.inputTitle = input_title_edit_record + "    #" + (index + 1);
+            $scope.petrol = petrol;
+            $scope.meter = meter;
+            $scope.rate = rate;
+        }
     }
 
     $scope.updateRecord = function ($event, petrol, meter, rate) {
-
+        $scope.input_edit_record = false;
         console.log("update called", petrol, meter, rate);
 
         $scope.records[$scope.recordIndex].petrol = petrol;
@@ -101,8 +123,10 @@ angular.module('starter.controllers', [])
         $scope.records[$scope.recordIndex].rate = rate;
 
         setCache();
-        $scope.popover.remove($event);
+        //$scope.popover.remove($event);
         $scope.recordIndex = -1;
+        $scope.clearFields();
+        switchInputToNewRecord();
     }
 
     $scope.deleteRecord = function (index) {
@@ -112,26 +136,15 @@ angular.module('starter.controllers', [])
         $anchorScroll();
     }
 
-    //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function () {
-        $scope.popover.remove();
-    });
-    // Execute action on hide popover
-    $scope.$on('popover.hidden', function () {
-        // Execute action
-    });
-    // Execute action on remove popover
-    $scope.$on('popover.removed', function () {
-        // Execute action
-    });
-
     angular.element(document).ready(function () {
-        $scope.listHeight = {
-            'height': (window.innerHeight - angular.element(inputBox)[0].offsetHeight - 50) + 'px',
-            'bottom': angular.element(inputBox)[0].offsetHeight + 'px'
-        }
-    });
 
+        $scope.listHeight = {
+            'height': (window.innerHeight - angular.element(inputBox)[0].offsetHeight - 30) + 'px',
+            'bottom': angular.element(inputBox)[0].offsetHeight - 10 + 'px'
+        }
+        console.log($scope.listHeight);
+        console.log(window.innerHeight);
+    });
 })
 
 
@@ -152,13 +165,28 @@ angular.module('starter.controllers', [])
             "distance": distance,
             "mileage": distance / petrolInLiter
         });
-
-        console.log($scope.data);
-
     }
+
+    $scope.getAverage = function () {
+        var avg = 0;
+        $scope.data.forEach(function (thisEntry) {
+            avg += thisEntry.mileage;
+        });
+        return avg/$scope.data.length;
+    }
+
+    angular.element(document).ready(function () {
+        $scope.listScrollHeight = {
+            'height': window.innerHeight - angular.element(footerAnalytics)[0].offsetHeight - angular.element(listHeader)[0].offsetHeight - 44 + 'px',
+        }
+        console.log("innerHeight", window.innerHeight);
+        console.log("footer height", angular.element(footerAnalytics)[0].offsetHeight);
+        console.log("calculated list scroll height",$scope.listScrollHeight);
+    });
 })
 
 .controller('InfoCtrl', function ($scope, localStorageService) {
+
     var data = localStorageService.get("mywheels_bikeinfo");
 
     if (data === null) {
@@ -174,24 +202,6 @@ angular.module('starter.controllers', [])
 
 .controller('EditInfoCtrl', function ($scope, localStorageService) {
     var records = localStorageService.get("mywheels_bikeinfo");
-    //$scope.data = [];
 
-    //if (records === null) {
-    //    records = [];
-    //}
-
-    //for (var i = 0; i < records.length - 1; i++) {
-    //    var distance = records[i + 1].meter - records[i].meter;
-    //    var petrolInLiter = records[i].petrol / records[i].rate;
-
-    //    $scope.data.push({
-    //        "petrol": petrolInLiter,
-    //        "distance": distance,
-    //        "mileage": distance / petrolInLiter
-    //    });
-
-    //    console.log($scope.data);
-
-    //}
 });
 
